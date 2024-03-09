653b2575ad3ef45d6f7aeb4b1de6cf28bb1eb5f6
FSSRepo <go778sgt@gmail.com>
2024-03-08 20:21:36 -0500

    update test

--- run-server.sh
+++ run-server.sh
@@ -5,7 +5,7 @@ wget https://github.com/Kitware/CMake/releases/download/v3.29.0-rc1/cmake-3.29.0
 tar -xzvf cmake-3.29.0-rc1.tar.gz
 cd cmake-3.29.0-rc1 && ./bootstrap && make -j$(nproc) && make install
 cd ../llama.cpp/models
-wget https://huggingface.co/ikawrakow/mixtral-instruct-8x7b-quantized-gguf/resolve/main/mixtral-instruct-8x7b-q2k.gguf
+wget https://huggingface.co/TheBloke/Mixtral-8x7B-v0.1-GGUF/resolve/main/mixtral-8x7b-v0.1.Q6_K.gguf
 mkdir ../build && cmake -S .. -B ../build -DLLAMA_CUBLAS=ON
 cmake --build ../build --config Release
-../build/bin/server -m mixtral-instruct-8x7b-q2k.gguf --port 8081 -c 512 --host 172.17.0.5 -ngl 32 --log-format text
+../build/bin/server -m mixtral-8x7b-v0.1.Q6_K.gguf --port 8081 -c 2048 --host 172.17.0.2 -ngl 33 --log-format text
